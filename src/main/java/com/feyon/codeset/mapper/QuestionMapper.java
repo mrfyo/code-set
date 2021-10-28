be3b0738be7be421e5ec290c5bfc0d1eb260a879
feng yong <mrfengyong@gmail.com>
2021-10-28 21:31:53 +0800

    ORM: add QuestionMapper.containsTag and QuestionMapper.holdStatus

--- src/main/java/com/feyon/codeset/mapper/QuestionMapper.java
+++ src/main/java/com/feyon/codeset/mapper/QuestionMapper.java
@@ -1,12 +1,43 @@
 package com.feyon.codeset.mapper;
 
+import com.feyon.codeset.common.Pageable;
 import com.feyon.codeset.entity.Question;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * ORM: Question
+ *
  * @author Feng Yong
  */
 @Mapper
 public interface QuestionMapper extends BaseMapper<Question> {
+    /**
+     * paging query.
+     *
+     * @param pageable {@link Pageable}
+     * @return question list.
+     */
+    List<Question> listAllByPage(Pageable pageable);
+
+    /**
+     * return the question is or not contains any tag.
+     *
+     * @param questionId question's id.
+     * @param tagIds     the list of id of list.
+     * @return the question is or not contains any tag.
+     */
+    boolean containsTag(@Param("questionId") Integer questionId, @Param("tagIds") List<Integer> tagIds);
+
+    /**
+     * return the question is or not has same status for this user.
+     *
+     * @param questionId question's id.
+     * @param userId     user' id
+     * @param status user's status
+     * @return the question is or not has same status for this user.
+     */
+    boolean holdStatus(@Param("questionId") Integer questionId, @Param("userId") Integer userId, @Param("status") Integer status);
 }
---
+++ src/test/java/com/feyon/codeset/service/QuestionServiceTest.java
@@ -0,0 +1,27 @@
+package com.feyon.codeset.service;
+
+import com.feyon.codeset.query.QuestionQuery;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
+class QuestionServiceTest {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @Test
+    void listAll() {
+        var query = new QuestionQuery();
+        query.setDifficulty(1);
+        query.setStatus(2);
+        query.setPage(1);
+        query.setSize(10);
+        questionService.listAll(query).forEach(System.out::println);
+    }
+}
