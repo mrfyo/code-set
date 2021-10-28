05817ca22c76ab7d93b5c19336d30e4784b143f6
feng yong <mrfengyong@gmail.com>
2021-10-28 15:59:16 +0800

    ORM: Submission pass

---
+++ src/main/java/com/feyon/codeset/mapper/SubmissionMapper.java
@@ -0,0 +1,11 @@
+package com.feyon.codeset.mapper;
+
+import com.feyon.codeset.entity.Submission;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Feng Yong
+ */
+@Mapper
+public interface SubmissionMapper extends BaseMapper<Submission> {
+}
---
+++ src/test/java/com/feyon/codeset/mapper/SubmissionMapperTest.java
@@ -0,0 +1,63 @@
+package com.feyon.codeset.mapper;
+
+import com.feyon.codeset.entity.Submission;
+import com.feyon.codeset.entity.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
+class SubmissionMapperTest {
+
+    @Autowired
+    private SubmissionMapper mapper;
+
+    @Test
+    void insert() {
+        var submission = new Submission();
+        submission.setResult(1);
+        submission.setQuestionId(1);
+        submission.setCreateAt(LocalDateTime.now());
+        assertEquals(1, mapper.insert(submission));
+    }
+
+    @Test
+    void update() {
+        var submission = mapper.findById(1);
+        assertNotNull(submission);
+        submission.setResult(1);
+        submission.setQuestionId(2);
+        submission.setCreateAt(LocalDateTime.now());
+        assertEquals(1, mapper.update(submission));
+    }
+
+    @Test
+    void deleteById() {
+        assertEquals(1, mapper.deleteById(1));
+    }
+
+    @Test
+    void findById() {
+        var submission = mapper.findById(1);
+        System.out.println(submission);
+        assertNotNull(submission);
+        assertEquals(2, submission.getResult());
+        assertEquals(1, submission.getQuestionId());
+        assertNotNull(submission.getCreateAt());
+        assertEquals(2021, submission.getCreateAt().getYear());
+    }
+
+    @Test
+    void findAll() {
+        List<Submission> list = mapper.findAll();
+        assertTrue(list.size() != 0);
+        list.forEach(System.out::println);
+    }
+}
