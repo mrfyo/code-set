ff9bd83c13c13c5c451db32eb203f9254dfb4667
feng yong <mrfengyong@gmail.com>
2021-10-28 13:28:40 +0800

    init

---
+++ src/main/java/com/feyon/codeset/CodeSetApplication.java
@@ -0,0 +1,16 @@
+package com.feyon.codeset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Feng Yong
+ */
+@SpringBootApplication
+public class CodeSetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/feyon/codeset/CodeSetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feyon.codeset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
