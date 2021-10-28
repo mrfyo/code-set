b25b5e918b43aaf00754afefdc2b652f5823ee22
feng yong <mrfengyong@gmail.com>
2021-10-28 14:00:12 +0800

    config database

--- src/test/java/com/feyon/codeset/CodeSetApplicationTests.java
+++ src/test/java/com/feyon/codeset/CodeSetApplicationTests.java
@@ -1,13 +1,21 @@
 package com.feyon.codeset;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class CodeSetApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        assert dataSource.getConnection() != null;
     }
 
 }
