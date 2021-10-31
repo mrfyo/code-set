8b70b7a7941b6c9360207fa65ab5953aac7526f0
feng yong <mrfengyong@gmail.com>
2021-10-31 09:54:18 +0800

    ORM: SolutionDetailMapper

---
+++ src/main/java/com/feyon/codeset/entity/SolutionDetail.java
@@ -0,0 +1,17 @@
+package com.feyon.codeset.entity;
+
+import lombok.Data;
+
+/**
+ * @author Feng Yong
+ */
+@Data
+public class SolutionDetail {
+
+    /**
+     * @see Solution id
+     */
+    private Integer id;
+
+    private String content;
+}
---
+++ src/main/java/com/feyon/codeset/mapper/SolutionDetailMapper.java
@@ -0,0 +1,11 @@
+package com.feyon.codeset.mapper;
+
+import com.feyon.codeset.entity.SolutionDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Feng Yong
+ */
+@Mapper
+public interface SolutionDetailMapper extends BaseMapper<SolutionDetail> {
+}
